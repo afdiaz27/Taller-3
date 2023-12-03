b58db457f9a9cd411ddf8257b763640031543d9e
Javier Farfán López <dj.farfan10@uniandes.edu.co>
2023-12-03 17:46:16 -0500

    Update Modelos_Clasificación.R

--- scripts/Modelos_Clasificación.R
+++ scripts/Modelos_Clasificación.R
@@ -288,3 +288,142 @@ write.csv(submit,file = "../Bases de datos - Versión 4/lasso_2_clasificación.c
 
 ### Lasso 3 con roc
 
+lasso_roc1 <- train(
+  modelo3, 
+  data = training, 
+  method = "glmnet",
+  trControl = Control,
+  family = "binomial", 
+  metric = "ROC",
+  tuneGrid = expand.grid(alpha = 0,lambda=grid), 
+  preProcess = c("center", "scale")
+)
+
+lasso_roc1
+
+p_load(pROC)
+resultados <- data.frame(Pobre=evaluation$Pobre)
+
+resultados$R1 <- predict(lasso_roc1,
+                         newdata=evaluation,
+                         type="prob")[,1]
+
+#ROC para el lasso
+
+rf_ROC1 <- roc(resultados$Pobre, resultados$R1, levels = rev(levels(resultados$Pobre)))
+rf_ROC1
+
+#Punto de corte
+
+rf_Thresh1 <- coords(rf_ROC1, x = "best", best.method = "closest.topleft")
+rf_Thresh1
+
+#Se evalúan resultados
+resultados<-resultados %>% mutate(lasso1hat05=ifelse(resultados$R1>0.5,"Si","No"),
+                                  lasso1rf_Thresh=ifelse(resultados$R1>rf_Thresh1$threshold,"Si","No"))
+
+# Caso en el que el threshold es = 0.5 (Bayes)
+with(resultados,table(Pobre,lasso1hat05))
+
+# Caso en el que el threshold se obtiene del ROC
+with(resultados,table(Pobre,lasso1rf_Thresh))
+
+testR <- data.frame(Pobre=testing$Pobre)
+testR
+
+testR$lasso_roc1 <- predict(lasso_roc1,
+                            newdata = testing,
+                            type= "prob")[,1]
+testR <- testR %>% 
+  mutate(
+    lasso_roc1=ifelse(lasso_roc1>rf_Thresh1$threshold,"si","no"),
+  )
+
+with(testR,table(Pobre,lasso_roc1))
+
+lasso_roc1
+
+d_submit <- df_test_hogares_2
+d_submit$predict <- predict(lasso_roc1,d_submit,type = "prob")[,1]
+submit <- d_submit %>% 
+  mutate(Pobre=ifelse(predict>0.5,1,0)) %>% 
+  select(id,Pobre)
+prop.table(table(submit$Pobre))
+write.csv(submit,file = "../Bases de datos - Versión 4/lasso3_ROC_clasificación.csv",row.names = FALSE)
+
+##### Elastic Net
+
+elasticnet1 <- train(
+  modelo3,
+  data = training,
+  method = "glmnet",
+  trControl = Control,
+  family = "binomial",
+  metric = "Acc",
+  preProcess = c("center", "scale")
+)
+
+elasticnet1
+
+testR <- data.frame(Pobre=testing$Pobre)
+
+testR$elasticnet1 <- predict(elasticnet1,
+                             newdata = testing,
+                             type = "prob")[,1]
+
+testR <- testR %>% 
+  mutate(
+    elasticnet1=ifelse(elasticnet1>0.5,"Si","No")
+  )
+
+with(testR,table(Pobre,elasticnet1))
+
+elasticnet1
+
+d_submit <- df_test_hogares_2
+d_submit$predict <- predict(elasticnet1,d_submit,type = "prob")[,1]
+submit <- d_submit %>% 
+  mutate(Pobre=ifelse(predict>0.5,1,0)) %>% 
+  select(id,Pobre)
+prop.table(table(submit$Pobre))
+write.csv(submit,file = "../Bases de datos - Versión 4/elastic_net_1.csv",row.names = FALSE)
+
+##### se modifica el tamaNo de la grilla
+
+##### Lasso con modelo 3
+
+
+grid2 <- 10^seq(-4, 0.01, length = 500)
+
+lasso4 <- train(
+  modelo3,
+  data = training,
+  method = "glmnet",
+  trControl = Control,
+  family = "binomial",
+  metric = "Sens",
+  tuneGrid = expand.grid(alpha = 0,lambda=grid2),
+  preProcess = c("center", "scale")
+)
+
+lasso1
+
+testR <- data.frame(Pobre=testing$Pobre)
+testR$lasso1 <- predict(lasso1,
+                        newdata = testing,
+                        type= "prob")[,1]
+
+testR <- testR %>% 
+  mutate(
+    lasso1=ifelse(lasso1>0.8,"si","no")
+  )
+
+with(testR,table(Pobre,lasso1))
+
+d_submit <- df_test_hogares_2
+d_submit$predict <- predict(lasso1,d_submit,type = "prob")[,1]
+submit <- d_submit %>% 
+  mutate(Pobre=ifelse(predict>0.8,1,0)) %>% 
+  select(id,Pobre)
+prop.table(table(submit$Pobre))
+write.csv(submit,file = "../Bases de datos - Versión 4/lasso_1_clasificación.csv",row.names = FALSE)
